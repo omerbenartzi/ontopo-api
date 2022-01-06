2a76e04cfb539f324ab78a837547dbeff6579946
Omer Ben Artzi <omerb12@gmail.com>
2022-01-06 20:45:38 +0200

    updated some stuff

--- ontopo.py
+++ ontopo.py
@@ -3,12 +3,14 @@
 Usage:
   ontopo-api.py find -m <month> -s <size> -t <time> -r <restaurant>
 """
+from typing import List, Tuple
 import docopt
 from calendar import monthrange
-from ontopo_api import search_available_table
+from api import search_available_table
 
-def find_in_month(month: int, size: int, time: str, restaurant: str):
+def find_in_month(month: int, size: int, time: str, restaurant: str) -> List[Tuple[str]]:
     month_days = monthrange(2022, month)
+    found = []
 
     if month < 10:
         month = "0" + str(month)
@@ -20,16 +22,26 @@ def find_in_month(month: int, size: int, time: str, restaurant: str):
         day = str(i)
         if i < 10:
             day = "0" + str(i)
+        print(f"Searching for {day}/{month}", end="\r")
         date = "2022" + month + day
         ans = search_available_table(restaurant, 
             {"size": size, "date": date, "time": time})
         if ans["method"] not in ["standby", "disabled"]:
-            print("Found a table at " + day + "/" + month + " at " + time + " for " + size + " people")
+            found.append((day, month, time, size))
+
+    return found
 
 def main():
     args = docopt.docopt(__doc__)
     if args["find"]:
-        find_in_month(int(args["<month>"]), int(args["<size>"]), args["<time>"], restaurant=args["<restaurant>"])
+        found = find_in_month(int(args["<month>"]), int(args["<size>"]), args["<time>"], restaurant=args["<restaurant>"])
+        
+        for day, month, time, size in found:
+            hours, min = time[:2], time[2:]
+            print(f"Found {day}/{month} at {hours}:{min} for {size} people")
+            
+        if not found:
+            print("No tables found")
     
 
 
